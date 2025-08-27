db8903d09685556319f971dbb03a3740642d9231
Gedion Adamu <gedionadamu@gmail.com>
2025-08-27 13:17:00 -0400

    Connecting Backend to DB

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const PORT = process.env.PORT ?? 8000
+const express = require('express')
+const app = express()
+const pool = require("./db")
+const cors = require('cors')
+app.use(cors())
+
+app.get('/',(req, res)=>{
+    res.send(`Server running on Port ${PORT}`)
+})
+//get all todos
+app.get('/todos/:userEmail', async(req,res)=>{
+    console.log(req.params)
+    const userEmail  = req.params.userEmail
+    try{
+      const todos =  await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
+      res.json(todos.rows)
+    }catch(err){
+        console.error(err)
+    }
+})
+
+app.listen(PORT, ()=> console.log(`Server running on Port ${PORT}`))
